587b618c738fd0703b32ec160f6e2417e5f85022
kobe <dev.skyachieve91@gmail.com>
2024-01-23 17:11:58 +0900

    :heavy_minus_sign: Build: npm 모듈 전체 삭제
    express, mongooes, cors, dotenv, http, socket.io 삭제후 재설치.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // express를 활용하여 서버 생성.
-const express = require("express")
-const mongoose = require("mongoose") // DB와 연결을 도와주는 mongoose 가져오기.
-const app = express() // express를 사용하여 app 생성.
+const express = require("express");
+const mongoose = require("mongoose"); // DB와 연결을 도와주는 mongoose 가져오기.
+require('dotenv').config();// .env 가져오기
+const cors = require("cors");// front-end의 무작위 접근을 막기 위한 cors 
+const app = express(); // express를 사용하여 app 생성.
+app.use(cors()); // 어떤 접근이든 허용?
 
 // mongoose를 활용하여 연결
 /*
@@ -9,9 +12,9 @@ mongoose.connect("DB ADDRESS가 들어가면 됨", { 옵션 })
 */ 
 
 // DB 주소 같은 경우, 중요한 변수이므로 따로 .env에 저장하여 관리함.
-mongoose.connect("", {
+mongoose.connect(process.env.DB, {
   userNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(()=>console.log("connected to database"));
 
-module.exports = app
+module.exports = app;
