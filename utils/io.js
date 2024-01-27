4f8536eefccf68be7d5a5d5896c0cba9dcbf98b5
kobe <dev.skyachieve91@gmail.com>
2024-01-27 11:17:43 +0900

    :white_check_mark: Build: socket callback 함수 구현 및 테스트.

--- utils/io.js
+++ utils/io.js
@@ -9,6 +9,11 @@ module.exports = function (io) {
   io.on("connection", async(socket) => { // 연결된 것을 socket이라고 부른다, 연결된 사람의 정보를 매개변수로 보내줌(socket)
     console.log("client is connected", socket.id);
 
+		socket.on("login", async(userName, callBackFunc) => {
+			console.log("back-end", userName);
+			
+		}); // 프론트에서 지정한 "login"을 들어준다. async 처리한 콜백 함수를 활용 response로 받은 유저 이름과 콜백 함수를 받아와 처리한다.
+
 		socket.on("disconnect", () => {
 			console.log("user is disconnected")
 		}); // 소켓 연결이 끊긴 상황을 듣는다.
